be7b390604a1ed3725185b3042f4873d073b4c33
bclaud <bruarrais@gmail.com>
2021-09-02 17:25:32 -0300

    adicionado requestMapping

--- users/src/main/java/com/usermanagement/controller/UserController.java
+++ users/src/main/java/com/usermanagement/controller/UserController.java
@@ -11,25 +11,27 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("api/v1/users")
 public class UserController {
 
     @Autowired
     private UserService service;
     
-    @PostMapping("api/v1/users")
+    @PostMapping
     public ResponseEntity<User> addUser(@RequestBody User user){
         return ResponseEntity.ok(service.insert(user));
     }
 
-    @GetMapping("/api/v1/users")
+    @GetMapping
     public ResponseEntity<List<User>> allUsers(){
         return ResponseEntity.ok(service.findAll());
     }
 
-    @GetMapping("/api/v1/users/{id}")
+    @GetMapping("/{id}")
     public User userById(@PathVariable long id){
         return service.findById(id);
     }
