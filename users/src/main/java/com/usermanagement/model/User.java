a7a9b9ceb9348e651b5232f9d9a7f5fe943af487
Bruno Arrais Claudino <bruarrais@gmail.com>
2021-09-24 16:48:31 -0300

    adicionado dependencia do lombok e realizados testes iniciais

--- users/src/main/java/com/usermanagement/model/User.java
+++ users/src/main/java/com/usermanagement/model/User.java
@@ -5,7 +5,10 @@ import java.io.Serializable;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
+import lombok.Data;
+
 @Entity
+@Data
 public class User implements Serializable {
     
     private static final long serialVersionUID = 1L;
@@ -16,104 +19,5 @@ public class User implements Serializable {
     private String surname;
     private String address;
     private Long securityCode;
-
-    public User () {
-    }
-
-    public User( String name, String surname) {
-        this.name = name;
-        this.surname = surname;
-    }
-
-    public User( String name, String surname, String address) {
-        this.name = name;
-        this.surname = surname;
-        this.address = address;
-    }
-
-    public User ( Long id, String name, String surname){
-        this.id = id;
-        this.name = name;
-        this.surname = surname;
-    }
-
-    public User ( Long id, String name, String surname, long securityCode){
-        this.id = id;
-        this.name = name;
-        this.surname = surname;
-        this.securityCode = securityCode;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id){
-        this.id = id;
-    }
-
-    public Long getSecurityCode(){
-        return securityCode;
-    }
-
-    public void setSecurityCode(Long securityCode){
-        this.securityCode = securityCode;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getSurname() {
-        return surname;
-    }
-
-    public void setSurname(String surname) {
-        this.surname = surname;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((name == null) ? 0 : name.hashCode());
-        result = prime * result + ((surname == null) ? 0 : surname.hashCode());
-        return result;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null)
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
-        User other = (User) obj;
-        if (name == null) {
-            if (other.name != null)
-                return false;
-        } else if (!name.equals(other.name))
-            return false;
-        if (surname == null) {
-            if (other.surname != null)
-                return false;
-        } else if (!surname.equals(other.surname))
-            return false;
-        return true;
-    }
-
     
 }
